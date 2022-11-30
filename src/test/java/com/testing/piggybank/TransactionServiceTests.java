5c81d38c2d933e8ab7a0697d9483cb757a06212b
shinevision <ShinevisionSV@Gmail.com>
2022-11-30 09:47:28 +0100

    Fix tests.

--- src/test/java/com/testing/piggybank/TransactionServiceTests.java
+++ src/test/java/com/testing/piggybank/TransactionServiceTests.java
@@ -18,6 +18,7 @@ public class TransactionServiceTests {
     public TransactionServiceTests() {
         this.transactionService = new TransactionService(null, null, null);
     }
+
     @Test
     public void filterAndLimitTransactionsTests() {
         final Account account = new Account();
@@ -29,24 +30,39 @@ public class TransactionServiceTests {
         final Account accountOtherOther = new Account();
         accountOtherOther.setId(3L);
 
-        List<Transaction> transactions = new ArrayList<>();
+        final Account accountNoTransactions = new Account();
+        accountOtherOther.setId(4L);
 
+        
         final Transaction transaction = new Transaction();
         transaction.setSenderAccount(account);
         transaction.setReceiverAccount(accountOther);
         transaction.setAmount(new BigDecimal(1000));
 
+        final Transaction transactionTwo = new Transaction();
+        transactionTwo.setSenderAccount(account);
+        transactionTwo.setReceiverAccount(accountOther);
+        transactionTwo.setAmount(new BigDecimal(1000));
+
 
         final Transaction transactionOther = new Transaction();
-        transaction.setSenderAccount(accountOther);
-        transaction.setReceiverAccount(accountOtherOther);
-        transaction.setAmount(new BigDecimal(1000));
-
+        transactionOther.setSenderAccount(accountOther);
+        transactionOther.setReceiverAccount(accountOtherOther);
+        transactionOther.setAmount(new BigDecimal(1000));
+        
+        final List<Transaction> transactions = new ArrayList<>();
         transactions.add(transaction);
+        transactions.add(transactionTwo);
         transactions.add(transactionOther);
 
-        List<Transaction> results = transactionService.filterAndLimitTransactions(transactions, account.getId(), null);
+        List<Transaction> resultsFirstAccount = transactionService.filterAndLimitTransactions(transactions, account.getId(), 10);
+        List<Transaction> resultsFirstAccountLimitedToOne = transactionService.filterAndLimitTransactions(transactions, account.getId(), 1);
+        List<Transaction> resultsSecondAccount = transactionService.filterAndLimitTransactions(transactions, accountOtherOther.getId(), 10);
+        List<Transaction> resultsEmptyAccount = transactionService.filterAndLimitTransactions(transactions, accountNoTransactions.getId(), 10);
 
-        Assertions.assertEquals(1, results.size());
+        Assertions.assertEquals(2, resultsFirstAccount.size());
+        Assertions.assertEquals(1, resultsFirstAccountLimitedToOne.size());
+        Assertions.assertEquals(1, resultsSecondAccount.size());
+        Assertions.assertEquals(0, resultsEmptyAccount.size());
     }
 }
